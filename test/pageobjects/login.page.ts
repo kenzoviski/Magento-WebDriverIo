a5227c5ed31c03a3f572704b5eab64e03cddf14f
TA <tiago.armando@foodintech.pt>
2024-02-20 16:08:11 +0000

    Prettier reconfig

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -1,39 +1,39 @@
-import Page from './page.js';
-import { $ } from '@wdio/globals';
+import Page from "./page.js";
+import { $ } from "@wdio/globals";
 
 class LoginPage extends Page {
   //Locators
 
   get userName() {
-    return $('#email');
+    return $("#email");
   }
 
   get password() {
-    return $('#pass');
+    return $("#pass");
   }
 
   get btnSignIn() {
-    return $('#send2');
+    return $("#send2");
   }
 
   get myAccount() {
-    return $('#maincontent > div.columns > div.column.main > div.page-title-wrapper > h1 > span');
+    return $("#maincontent > div.columns > div.column.main > div.page-title-wrapper > h1 > span");
   }
 
   get accountInformations() {
     return $(
-      '#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-title > strong',
+      "#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-title > strong",
     );
   }
   get contactInformation() {
     return $(
-      '#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-content > div > strong > span',
+      "#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-content > div > strong > span",
     );
   }
 
   get contactInfo() {
     return $(
-      '#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-content > div > div.box-content',
+      "#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-content > div > div.box-content",
     );
   }
 
